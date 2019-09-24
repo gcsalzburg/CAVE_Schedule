50b556885813cad1f85c0067d49146c45e3d0d62
gcsalzburg <32631912+gcsalzburg@users.noreply.github.com>
2019-09-24 09:26:04 +0200

    Convert to a class

--- CAVE_Schedule.cpp
+++ CAVE_Schedule.cpp
@@ -1,52 +1,36 @@
-/*
- * kTasks.cpp
- *
- *  Created on: 4 Jun 2018
- *      Author: g.cave
- */
-
-
-#include "CAVE_Schedule.h"
 #include "Arduino.h"
+#include "CAVE_Schedule.h"
 
-#define ARRAYSIZE(a) (sizeof(a)/sizeof(a[0]))
+CAVE::Task* tasks = 0;
+uint16_t 	num_tasks = 0;
 
-LoopTimer_t* tasks = 0;
-uint16_t numtasks = 0;
+CAVE::Task::Task(){};
 
-// Private functions
-void SampleTask(struct LoopTimer* t);
+CAVE::Task::Task(uint32_t d) :
+	delay(d)
+{};
 
-LoopTimer_t sample = {0, 50, 0, SampleTask};
+CAVE::Task::Task(void(*callbackPointer)(void), uint32_t d){
+	delay = d;
+	callback = callbackPointer;
+};
 
-
-void tasks_init(void){
-	// Init here
+void CAVE::Task::fire(uint32_t ts){
+	callback();
+	last_fired = ts;
 }
 
-void tasks_RegisterTaskList(LoopTimer_t* tasktable, uint16_t count){
-	tasks = tasktable;
-	numtasks = count;
+void CAVE::tasks_register_with_cnt(Task* task_table, uint16_t cnt){
+	tasks = task_table;
+	num_tasks = cnt;
 }
 
-void tasks_update(){
+void CAVE::tasks_update(void){
 	// Task handling
 	uint32_t curr_ms = millis();
-	for(uint16_t i = 0; i < numtasks; i++){
-		if(curr_ms - tasks[i].last > tasks[i].delay){
-			tasks[i].fire = 1;
-			tasks[i].last = curr_ms;
+	for(uint16_t i = 0; i < num_tasks; i++){
+		if(curr_ms - tasks[i].last_fired > tasks[i].delay){
+			tasks[i].fire(millis());
 		}
 	}
-	for(uint16_t i = 0; i < numtasks; i++){
-		if(tasks[i].fire){
-			tasks[i].callback(&tasks[i]);
-			tasks[i].fire = 0;
-		}
-	}
-}
-
-
-void SampleTask(struct LoopTimer* t){
-	Serial.print("Sample task trigger");
 }
--- CAVE_Schedule.h
+++ CAVE_Schedule.h
@@ -1,28 +1,32 @@
-/*
- * kTasks.h
- *
- *  Created on: 4 Jun 2018
- *      Author: g.cave
- */
-
 #ifndef CAVE_SCHEDULE_H_
 #define CAVE_SCHEDULE_H_
 
 #include "Arduino.h"
 
-// Loop timer
-typedef struct LoopTimer{
-	uint32_t last;		// Last time the function was fired
-	uint32_t delay;		// Interval between each call to this task
-	uint8_t fire;		// Flag for task handler to determine whether to call or not
-	void (*callback)(struct LoopTimer* t);	// Callback function to fire
-}LoopTimer_t;
+namespace CAVE{
+
+	class Task{
+
+		public:
+			Task();
+			Task(uint32_t d);
+
+			Task(void(*callbackPointer)(void), uint32_t d);
+
+			uint32_t delay;
+			uint32_t last_fired;
+
+			void fire(uint32_t ts);
+
+			void(*callback)(void);
+	};
+
+	void tasks_register_with_cnt(Task* task_table, uint16_t cnt);
+	void tasks_update(void);
+};
 
 
-void tasks_init(void);
-void tasks_update(void);
-void tasks_RegisterTaskList(LoopTimer_t* tasklist, uint16_t count);
-
-#define tasks_register(c) (tasks_RegisterTaskList(c, (sizeof(c)/sizeof(c[0]))))
+// Redefine tasks_register to include array size
+#define tasks_register(c) tasks_register_with_cnt(c, (sizeof(c)/sizeof(c[0])) )
 
 #endif /* CAVE_SCHEDULE_H_ */
